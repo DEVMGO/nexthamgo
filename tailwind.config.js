c77f28997c1392d0f5a57f7bf78a0e41752de286
Dev-Golmohammadi <92379327+DEVMGO@users.noreply.github.com>
2022-12-03 14:26:41 +0330

    edit install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        bgNavHome: 'rgba(38, 43, 62, 0.5)',
-      },
-      screens: {
-        res1: "1410px",
-        res2: "1230px",
-        res3: {'max': '640px'},
-        res4: {'max': '700px'},
-        res5: {'max': '900px'},
-      },
-      boxShadow: {
-        normal: "0px 0px 10px 0px rgba(0, 0, 0, .2)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
